34551e5cbdc1051e6b9c2d1bc83572fb6176d3e7
nickmc5 <nick.mcclure55@gmail.com>
2023-11-02 13:59:43 -0400

    added props and fixed export

--- client/src/components/Reporting/Flagging.jsx
+++ client/src/components/Reporting/Flagging.jsx
@@ -2,10 +2,16 @@ import React from 'react';
 import { useState } from 'react/cjs/react.production.min';
 
 const [flags, setFlags] = useState(0);
+function handleClick() {
+    setFlags(flags++);
+    if (flags == 5) { // arbitrary number that can be adjusted
+        mute(user); // for unwritten mute function, should be adjusted as needed
+    }
+}
 
-export default function RouteButton() {
+export default function flagButton({user, mute}) {
     return (
-        <button type='button' onClick={setFlags(flags++)}>
+        <button type='button' onClick={() => handleClick}>
             Flag
         </button>
     );
