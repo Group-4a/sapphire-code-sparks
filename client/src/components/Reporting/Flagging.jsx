1d82787c5cac1ec26f09088c5d81156b8e0631cf
Ryan Jones <130319707+RcRyanJones@users.noreply.github.com>
2023-11-03 17:16:24 -0400

    Update Flagging.jsx

--- client/src/components/Reporting/Flagging.jsx
+++ client/src/components/Reporting/Flagging.jsx
@@ -3,10 +3,21 @@ import { BsFlag } from "react-icons/bs";
 
 export default function FlagButton({ user, classroomId }) {
   const [flags, setFlags] = useState(0);
+  let mutedUsers = [];
 
   function mute(user) {
     // mute user in backend stuff
+	
+    mutedUsers.push(user);
+    postFilter(user);
+    //muted users is list of users muted by the individual sending the funciton call
     console.log("user muted");
+  }
+
+  function postFilter(user){ //uses posts from gallery team
+     for (let i = 0; i < posts.length(); i++) {
+     posts = posts.filter(post => post.user.at(i) !== user);
+    } //modify currently unimplemented post array that displays user posts to not include muted users  
   }
 
   function handleClick() {
