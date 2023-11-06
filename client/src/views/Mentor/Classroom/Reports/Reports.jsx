eb0b8754b24c6386a87711baaca926a903442827
andychen482 <andy.chen0607@gmail.com>
2023-11-05 19:09:44 -0500

    Added table to report page, fixed client side functionality

--- client/src/views/Mentor/Classroom/Reports/Reports.jsx
+++ client/src/views/Mentor/Classroom/Reports/Reports.jsx
@@ -1,11 +1,62 @@
 import React from "react";
 import FlagButton from "../../../../components/Reporting/Flagging";
+import { Table } from "antd";
 
 export default function Reports({ classroomId }) {
+  // Change to fetch reportData from database using classroomId
+  const reportsData = [
+    {
+      key: "1",
+      user: "John Doe",
+      reason: "Inappropriate language",
+    },
+    {
+      key: "2",
+      user: "Jane Smith",
+      reason: "Inappropriate thumbnail",
+    },
+    {
+      key: "3",
+      user: "Bill Bob",
+      reason: "Bullying",
+    },
+  ];
+
+  const columns = [
+    {
+      title: "User",
+      dataIndex: "user",
+      key: "user",
+    },
+    {
+      title: "Reason",
+      dataIndex: "reason",
+      key: "reason",
+    },
+    {
+      title: "Actions",
+      key: "actions",
+      render: (text, record) => (
+        <FlagButton
+          user={record.user}
+          classroomId={classroomId}
+          isAdmin={true}
+        />
+      ),
+    },
+  ];
+
   return (
-    <div>
-      <h1>Reports</h1>
-      <FlagButton id="flag" user={1234} classroomId={classroomId} />
+    <div className="reports-container">
+      <div id="page-header">
+        <h1>Reports</h1>
+      </div>
+      <div
+            id='content-creator-table-container'
+            style={{ marginTop: '6.6vh' }}
+          >
+      <Table dataSource={reportsData} columns={columns} />
+      </div>
     </div>
   );
 }
